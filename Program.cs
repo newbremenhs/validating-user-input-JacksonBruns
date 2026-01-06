1f69a2b23f2f903985187bffc8fb35a282889f02
JacksonBruns <28brunsj@newbremenschools.org>
2026-01-06 18:59:24 +0000

    done

--- Program.cs
+++ Program.cs
@@ -1,34 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.Urls.Add("http://localhost:5000");
-
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/{cityName}/weather", GetWeatherByCity);
-
-app.Run();
-
-
-Weather GetWeatherByCity(string cityName)
+Console.WriteLine("What is your favorite flavor of ice cream?");
+string? favorite = Console.ReadLine();
+if(favorite == null)
 {
-    app.Logger.LogInformation($"Weather requested for {cityName}.");
-    var weather = new Weather(cityName);
-    return weather;
+    favorite = "Vanilla";
 }
-
-public record Weather
+if(favorite == "vanilla");
 {
-    public string City { get; set; }
-
-    public Weather(string city)
-    {
-        City = city;
-        Conditions = "Cloudy";
-        // Temperature here is in celsius degrees, hence the 0-40 range.
-        Temperature = new Random().Next(0,40).ToString();
-    }
-
-    public string Conditions { get; set; }
-    public string Temperature { get; set; }
+    Console.WriteLine("Your basic");
 }
+else
+{
+    Console.WriteLine("Thats fruity")
+
+}
+Console.WriteLine($"Your favorite flavor is {favorite}.")
